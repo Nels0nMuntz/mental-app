7211817dd17aa807c7b902bfeaca93a04d31a69b
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2024-07-28 11:34:51 +0300

    eslint, prettier setup

---
+++ eslint.config.js
@@ -0,0 +1,98 @@
+import js from "@eslint/js";
+import globals from "globals";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tslint from "typescript-eslint";
+
+// For JS
+/** @type {import('eslint').Linter.FlatConfig} */
+// export default [
+//   {
+//     plugins: {
+//       react: eslintPluginReact,
+//       'react-hooks': eslintPluginReactHooks,
+//       'react-refresh': eslintPluginReactRefresh,
+//       prettier: eslintPluginPrettier,
+//     }
+//   },
+//   {
+//     ignores: ['node_modules', 'build', '.next']
+//   },
+//   js.configs.recommended,
+//   {
+//     languageOptions: {
+//       globals: {
+//         ...globals.node,
+//         ...globals.browser,
+//         ...globals.es2022,
+//       },
+//       parserOptions: eslintPluginReact.configs.recommended.parserOptions,
+//     }
+//   },
+//   {
+//     files: ['**/*.{js,jsx}'],
+//     rules: {
+//       ...eslintPluginPrettier.configs.,
+//       ...eslintConfigPrettier.rules,
+//       "no-unused-vars": "warn",
+//       "no-undef": "warn",
+//       'prefer-const': 'error',
+//       'react/jsx-no-target-blank': 'off',
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true }
+//       ]
+//     }
+//   }
+// ];
+
+// For TS
+export default tslint.config(
+  {
+    plugins: {
+      "@typescript-eslint": tslint.plugin,
+      react: eslintPluginReact,
+      "react-hooks": eslintPluginReactHooks,
+      "react-refresh": eslintPluginReactRefresh,
+      prettier: eslintPluginPrettier,
+    },
+  },
+  {
+    ignores: ["node_modules", "build", ".next", "*.config.js"],
+  },
+  js.configs.recommended,
+  ...tslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2022,
+      },
+      parserOptions: {
+        parser: tslint.parser,
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: '.'
+      },
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      ...eslintPluginPrettier.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      "no-unused-vars": "warn",
+      "prefer-const": "error",
+      "react/jsx-no-target-blank": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+);
